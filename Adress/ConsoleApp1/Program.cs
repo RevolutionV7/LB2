065b46badc2f326cfa599509ffbf154bfa1ad073
RevolutionV7 <sdvsvd11@gmail.com>
2022-09-17 15:49:02 +0300

    Second commit/finish

--- Adress/ConsoleApp1/Program.cs
+++ Adress/ConsoleApp1/Program.cs
@@ -4,9 +4,40 @@ namespace ConsoleApp1
 {
     class Adress
     {
-        static void Main(string[] args)
+            internal class Address
         {
-            C.
+            public string index;
+            public string country;
+            public string city;
+            public string street;
+            public string house;
+            public string apartment;
+
+            public void Print()
+            {
+                Console.WriteLine($"Index: {index};");
+                Console.WriteLine($"Country: {country};");
+                Console.WriteLine($"City: {city};");
+                Console.WriteLine($"Street: {street};");
+                Console.WriteLine($"House: {house};");
+                Console.WriteLine($"Apartment: {apartment}.");
+            }
         }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Address address = new Address();
+                address.index = "02156";
+                address.country = "Ukraine";
+                address.city = "Kyiv";
+                address.street = "Kioto";
+                address.house = "19";
+                address.apartment = "A";
+                address.Print();
+            }
+        }
+        
     }
 }
+    
