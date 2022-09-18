e7b59c45a38dbd3ab7a870227b991b65d2402492
RevolutionV7 <sdvsvd11@gmail.com>
2022-09-18 15:44:01 +0300

    the first programe changing

--- Adress/ConsoleApp1/Program.cs
+++ Adress/ConsoleApp1/Program.cs
@@ -12,7 +12,73 @@ namespace ConsoleApp1
             public string street;
             public string house;
             public string apartment;
+            public string Index
+            {
+                get
+                {
+                    return index;
+                }
+                set
+                {
+                    index = value;
+                }
+            }
 
+            public string Country
+            {
+                get
+                {
+                    return country;
+                }
+                set
+                {
+                    country = value;
+                }
+            }
+            public string City
+            {
+                get
+                {
+                    return city;
+                }
+                set
+                {
+                    city = value;
+                }
+            }
+            public string Street
+            {
+                get
+                {
+                    return street;
+                }
+                set
+                {
+                    street = value;
+                }
+            }
+            public string House
+            {
+                get
+                {
+                    return house;
+                }
+                set
+                {
+                    house = value;
+                }
+            }
+            public string Apartment
+            {
+                get
+                {
+                    return apartment;
+                }
+                set
+                {
+                    apartment = value;
+                }
+            }
             public void Print()
             {
                 Console.WriteLine($"Index: {index};");
